0c1e097296dfa58441c5609ac5345ab1fbb9f56a
James Randolph <james.randolph2016@gmail.com>
2025-01-24 10:05:40 -0500

    add model view, model, react three fiber, drei

---
+++ src/components/ModelView.tsx
@@ -0,0 +1,59 @@
+import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
+import * as THREE from 'three'
+import { RefObject } from 'react'
+import Lights from './Lights';
+import Loader from './Loader';
+import IPhone from './IPhone';
+import { Suspense } from "react";
+
+interface ModelViewProps {
+  index: number
+  groupRef: RefObject<THREE.Group>
+  gsapType: string
+  controlRef: any
+  setRotationState: (rotation: number) => void
+  size: string
+  item: {
+    title: string
+    color: string[]
+    img: string
+  }
+}
+
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }: ModelViewProps) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
+    >
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+      <OrbitControls 
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0 ,0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      /> 
+
+      <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0 ,0]}>
+        <Suspense fallback={<Loader />}>
+          <IPhone 
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
+    </View>
+  )
+}
+
+export default ModelView
