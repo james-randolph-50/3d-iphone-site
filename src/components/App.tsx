781fd69b031b0b23eda7beca1497e79318d06b91
James Randolph <james.randolph2016@gmail.com>
2025-01-24 08:11:05 -0500

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,20 @@
+import Avatar from 'components/Avatar'
+import logo from 'assets/logo.svg'
+
+const randoms = [
+  [1, 2],
+  [3, 4, 5],
+  [6, 7]
+]
+
+function App() {
+  return (
+    <>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">Welcome</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
